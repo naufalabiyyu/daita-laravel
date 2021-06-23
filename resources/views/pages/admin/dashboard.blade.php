17f3880cc57e7e12fbecb7b3aaf314246801af71
naufalabiyyu <naufalabiyyu@naufalabiyyu.local>
2021-06-23 21:57:34 +0700

    DELETE UNUSE FOLDER N CODING

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -11,47 +11,6 @@
                 <h2 class="dashboard-title">Admin Dashboard</h2>
                 <p class="dashboard-subtitle">this is daita skincare admin page!</p>
             </div>
-            {{-- <div class="dashboard-content">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="card mb-2">
-                            <div class="card-body">
-                                <div class="dashboard-card-title">
-                                    Customer
-                                </div>
-                                <div class="dashboard-card-subtitle">
-                                    {{ $customer }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card mb-2">
-                            <div class="card-body">
-                                <div class="dashboard-card-title">
-                                    Revenue
-                                </div>
-                                <div class="dashboard-card-subtitle">
-                                   Rp {{ $revenue }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card mb-2">
-                            <div class="card-body">
-                                <div class="dashboard-card-title">
-                                    Transaction
-                                </div>
-                                <div class="dashboard-card-subtitle">
-                                    {{ $transaction }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div> --}}
             <div class="rows">
                 <div class="column" style="margin-right: -100px">
                     <a href="{{ url('admin/user') }}" class="card-admin credentialing">
@@ -82,8 +41,6 @@
                         <p>Transaction</p>
                         <h2>{{ $transaction }}</h2>
                     </a>
-                </div>
-            </div>
                 </div>
             </div>
         </div>
